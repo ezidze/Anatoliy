f753c43385f9886951f04150601ddf80e876f77f
Maxim Raikovskyi <ezidze123@gmail.com>
2016-10-22 13:29:28 +0300

    Add files via upload

---
+++ genuino_101.ino
@@ -0,0 +1,37 @@
+
+const int pin_button = 6;
+
+const int pin_rele = 7;
+
+const int pin_datchik_sveta = 10;
+
+//функция ручного режима управления лампой
+void hands_mode( boolean light ){
+  if ( light ){
+    digitalWrite( pin_rele, LOW ); // не подаем напряжение на  реле
+  }
+  else{
+    digitalWrite( pin_rele, HIGH ); // подаем напряжение на реле 
+  }
+  
+
+}
+
+//функция автоматической работы лампы
+void auto_mode(){
+  int light = 0;
+  light = analogRead(pin_datchik_sveta);
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(pin_button, INPUT);
+pinMode(pin_rele, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
